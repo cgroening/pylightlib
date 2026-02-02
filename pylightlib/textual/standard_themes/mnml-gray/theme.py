1281c443eb437bb4bf26bcd7654f7233a5e1f0cf
Corvin Gröning <114770097+cgroening@users.noreply.github.com>
2026-02-02 14:58:22 +0100

    Add new theme mnml-gray

---
+++ pylightlib/textual/standard_themes/mnml-gray/theme.py
@@ -0,0 +1,23 @@
+from textual.theme import Theme
+
+
+TEXTUAL_THEME = Theme(
+    name='mnml-gray',
+    primary='#8BD3CD',
+    secondary='#444444',
+    accent='#A9B7C6',
+    foreground='#E5E5E5',
+    background='#222222',
+    surface='#444444',
+    panel="#242628",
+    boost='#444C5E',
+    success='#A3C995',
+    warning='#E6C384',
+    error='#D67B76',
+    dark=True,
+    variables={
+        'block-hover-background': "#335D48",
+        'footer-key-foreground': '#D2D05F',
+        'input-cursor-text-style': 'reverse',
+    }
+)
